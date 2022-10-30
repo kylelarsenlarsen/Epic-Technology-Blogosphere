d982de43f3305fe2167e954a1fa7c9828fb4d7c3
Kyle Larsen <kylelarsen.business@gmail.com>
2022-10-29 21:28:23 -0600

    building files to create seed functions.

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,6 @@
 const sequelize = require('../config/connection');
 const { User } = require('../models');
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
-
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
